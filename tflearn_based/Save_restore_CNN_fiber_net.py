e704e95325ec7aa82369402768e2c3fbfbe73155
Vikash Gupta <vikashg@hpc-login2-l.hpcc.usc.edu>
2017-07-18 15:48:23 -0700

    Basic architecture completed

---
+++ tflearn_based/Save_restore_CNN_fiber_net.py
@@ -0,0 +1,9 @@
+from __future__ import division, print_function, absolute_import
+import os, os.path
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.estimator import regression
+import sys
+import numpy as np  
