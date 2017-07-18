578017fe7aa1d2cc950c096e34ef1a771e2cc818
Vikash Gupta <vgupta@c2001.c.ini.usc.edu>
2017-07-18 10:21:41 -0700

    added wrapper for job submissions

---
+++ PreProcessData/submit_jobs.sh
@@ -0,0 +1,6 @@
+base_dir=/ifs/loni/faculty/thompson/four_d/Faisal/Projects/NeuralNet_TBI/AD_DOD_vol_param/
+out_dir=/ifs/loni/faculty/thompson/four_d/Faisal/Projects/NeuralNet_TBI/AD_DOD_vol_param/Training_data_new/
+#sub-directory within the subject directory 
+sub_dir=/NewResults/Mapped_tracks/Tracks_text_files/
+
+python_dir=
---
+++ README.txt
@@ -0,0 +1,2 @@
+So, I am gonna write the codes for fibernet model based on tensorflow and not on tflearn
+
