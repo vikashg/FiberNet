35a23cb111843b83803ce01698f64e5cb8159403
Vikash Gupta <vikashg@hpc-login2-l.hpcc.usc.edu>
2017-07-18 16:36:31 -0700

    Temp changes

--- Job_Submission_scripts/submit_jobs.sh
+++ Job_Submission_scripts/submit_jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PBS -l walltime=4:00:00
-#PBS -o /home/rcf-proj2/vg/Softwares/FiberNet/PreProcessData/logs
+#PBS -l walltime=24:00:00
+#PBS -o /home/rcf-proj2/vg/Softwares/FiberNet/Job_Submission_scripts/logs
 #PBS -j oe
 #PBS -N "DataPrep"  
-apps_dir=/home/rcf-proj2/vg/Softwares/FiberNet/PreProcessData
+apps_dir=/home/rcf-proj2/vg/Softwares/FiberNet/tflearn_based
 base_dir=/home/rcf-proj2/vg/Data/AD_DOD/TrainingData/
 out_dir=${base_dir}
 
@@ -17,6 +17,6 @@ fiber_list=${apps_dir}/fiber_groups_reduced.txt
 python_CPU_dir=/home/rcf-proj2/vg/Softwares/CPU_version/miniconda3/bin
 python_GPU_dir=/home/rcf-proj2/vg/Softwares/miniconda3/bin
 
-# ${python_GPU_dir}/python3 ${apps_dir}/scratch_read_file.py $train_subj_list $out_dir $base_dir $sub_dir $fiber_list 
+${python_CPU_dir}/python3 ${apps_dir}/Save_restore_CNN_fiber_net.py  ${base_dir}
 
-${python_CPU_dir}/python3 ${apps_dir}/Create_train_test_split.py ${out_dir} ${split_ratio}
+
--- tflearn_based/Save_restore_CNN_fiber_net.py
+++ tflearn_based/Save_restore_CNN_fiber_net.py
@@ -17,7 +17,7 @@ def load_files(base_dir, split_name):
     labels = np.load(labels_name)
     return fibers, labels
     
-batch_size = 7500
+batch_size = 500
 num_points = 50
 num_fiber_bundles = 17
 num_epochs = 1 
@@ -25,12 +25,13 @@ base_dir = sys.argv[1]
 
 out_dir = base_dir + 'CNN_model/'
 if not os.path.exists(out_dir):
-    os,makedirs(out_dir) 
+    os.makedirs(out_dir) 
 
-
+print('Reading data')
 train_fibers, train_labels = load_files (base_dir, 'train')
 test_fibers, test_labels = load_files (base_dir, 'test')
 valid_fibers, valid_labels = load_files(base_dir, 'valid')
+print('Data reading complete')
 
 ## Network architecture
 network = input_data(shape = [None, num_points, 3, 1], name= 'input')
@@ -48,8 +49,8 @@ network = fully_connected(network, 256, activation = 'tanh')
 network = dropout(network, 0.8)
 
 network = fully_connected(network, num_fiber_bundles, activation = 'softmax')
-network = regression(network, optimizaer = 'adam', learning_rate = 0.0001, loss='categorical_crossentropy', name='target')
-
+network = regression(network, optimizer = 'adam', learning_rate = 0.0001, loss='categorical_crossentropy', name='target')
+print('Architecutr definition complete')
 ## Network architecture completed
 
 
@@ -57,9 +58,15 @@ network = regression(network, optimizaer = 'adam', learning_rate = 0.0001, loss=
 train_data = train_fibers.reshape([-1, num_points, 3, 1])
 valid_data = valid_fibers.reshape([-1, num_points, 3, 1])
 
+print(train_data.shape)
+print(valid_data.shape)
+
+print(train_data[1,:])
+print(valid_data[1,:])
+
 ## Start training
 model = tflearn.DNN(network, tensorboard_verbose = 0)
-mode_name = out_dir + 'CNN_conv2d_32_conv2d_64_fc_128_fc_256_epoch_' + str(num_epoch) + '.tflearn'
+mode_name = out_dir + 'CNN_conv2d_32_conv2d_64_fc_128_fc_256_epoch_' + str(num_epochs) + '.tflearn'
 
 model.fit({'input': train_data}, {'target': train_labels}, n_epoch= num_epochs,validation_set=({'input': valid_data}, {'target': valid_labels}), snapshot_step=100, show_metric=True, batch_size=3000, run_id='convnet_track')
 model.save(model_name)
